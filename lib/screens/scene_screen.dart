60c8066b30b2d7f815e33b1cf17023f1743bc164
dot-ruth <ruthgetaneh5@gmail.com>
2025-04-18 18:16:33 +0300

    feat: add start to the scene.json

--- lib/screens/scene_screen.dart
+++ lib/screens/scene_screen.dart
@@ -17,58 +17,57 @@ class SceneScreen extends StatelessWidget {
 
 final scene = gameState.currentScene;
 
-return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Resurrection',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w900
-            ),
+return SafeArea(
+  child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
           ),
-        backgroundColor: Colors.brown[300],
-        ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.brown[300],
-          image: DecorationImage(
-            opacity: 0.5,
-            image: AssetImage(scene!.imagePath),
-            fit: BoxFit.fill
-            ),
-        ),
-        child: Padding(
-          
-          padding: const EdgeInsets.all(50.0),
-          
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                scene.narrative, 
-                style: const TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600
-                  )
-                  ),
-              const SizedBox(height: 20),
-              ...scene.choices.map((choice) {
-                return ElevatedButton(
-                  onPressed: () {
-                    gameState.makeChoice(choice);
-                  },
-                  child: Text(
-                    choice.text,
-                    style: TextStyle(color: Colors.black),
+        body: Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.brown[300],
+            image: DecorationImage(
+              opacity: 0.5,
+              image: AssetImage(scene!.imagePath),
+              fit: BoxFit.fill
+              ),
+          ),
+          child: Padding(
+            
+            padding: const EdgeInsets.all(50.0),
+            
+            child: Column(
+              
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  scene.narrative, 
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600
+                    )
                     ),
-                );
-              })
-            ],
+                const SizedBox(height: 20),
+                ...scene.choices.map((choice) {
+                  return ElevatedButton(
+                    onPressed: () {
+                      gameState.makeChoice(choice);
+                    },
+                    child: Text(
+                      choice.text,
+                      style: TextStyle(color: Colors.black),
+                      ),
+                  );
+                })
+              ],
+            ),
           ),
         ),
       ),
-    );
+);
   }
 }
