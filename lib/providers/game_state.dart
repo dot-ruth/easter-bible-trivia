79a7d435192b5e7780f53639ff46e44b52905b53
dot-ruth <ruthgetaneh5@gmail.com>
2025-04-19 20:51:46 +0300

    refactor

--- lib/providers/game_state.dart
+++ lib/providers/game_state.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 import '../models/scene.dart';
-import 'dart:convert';
-import 'package:flutter/services.dart' show rootBundle;
 
 class GameState extends ChangeNotifier {
-  List<Scene> _scenes = [];
+  final List<Scene> _scenes = [];
   Scene? _currentScene;
   Scene? get currentScene => _currentScene;
-
-  Future<void> loadScenes() async {
-    final String jsonString = await rootBundle.loadString('lib/data/scene.json');
-    final List<dynamic> jsonList = json.decode(jsonString);
-    _scenes = jsonList.map((s) => Scene.fromJson(s)).toList();
-    _currentScene = _scenes.first;
-    notifyListeners();
-  }
 
   void makeChoice(Choice choice) {
     _currentScene = _scenes.firstWhere((s) => s.id == choice.outcomeSceneId);
